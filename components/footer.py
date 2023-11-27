787402d9402cfd7d88c47923ae5a0abbfcf30e25
KonorOko <125145871+KonorOko@users.noreply.github.com>
2023-11-27 03:19:14 +0000

    Arreglando el gitignore

--- components/footer.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-class Footer:
-    def builder(self):
-        st.divider()
-        st.markdown("#### Ganthaus")
-        st.text("Necesitas ayuda? Contacta a")
-        st.text("© 2023 - Todos los derechos reservados")
